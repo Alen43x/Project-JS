5ac1f39ff2627f269658b632532e08fd350d8df4
Alen43x <Alen43x@gmail.com>
2022-09-29 20:43:22 -0300

    se agrega el proyecto

---
+++ main.js
@@ -0,0 +1,35 @@
+/*let n1 = parseInt(prompt("ingrese un numero"));
+let n2 = parseInt(prompt("ingrese otro numero"));
+let op = prompt("Ingrese una operacion");
+
+
+function calculadora(n1, n2, op) {
+    switch (op) {
+        case "+" :
+            return n1 + n2;
+            break;
+        case "-" :
+            return n1 - n2;
+            break;
+        case "*" :
+            return n1 * n2;
+            break;
+        case "/" :
+            return n1 / n2;
+            break;
+        default:
+            return 0;
+            break;
+    }
+}
+
+let resultado = calculadora (n1, n2, op);
+alert(resultado); */
+
+function Producto1 (nombre, precio, calidad) {
+    this.nombre = "BigMac";
+    this.precio = 600;
+    this.calidad = "5 estrellas";
+}
+
+console.log(Producto1)
